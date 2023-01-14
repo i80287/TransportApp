f494364cec1d1a1adb3a8fb07d983d95d6945199
i80287 <92759021+i80287@users.noreply.github.com>
2023-01-14 22:00:53 +0300

    update TransportException comments

--- EKRLib/TransportException.cs
+++ EKRLib/TransportException.cs
@@ -1,16 +1,32 @@
 namespace EKRLib
 {
+    /// <summary>
+    /// Represents a class for the <see cref="EKRLib"/> library special exceptions.
+    /// </summary>
     public sealed class TransportException : System.Exception
     {
+        /// <summary>
+        /// Initializes a new <see cref="TransportException"/> with empty message.
+        /// </summary>
         public TransportException()
         {
         }
 
+        /// <summary>
+        /// Initializes a new <see cref="TransportException"/> with <paramref name="message"/> message.
+        /// </summary>
+        /// <param name="message">Exception message.</param>
         public TransportException(string message)
             : base(message)
         {
         }
 
+        /// <summary>
+        /// Initializes a new <see cref="TransportException"/> with <paramref name="message"/> message
+        /// and inner exception <paramref name="inner"/> for the stack trace.
+        /// </summary>
+        /// <param name="message">Exception message.</param>
+        /// <param name="inner">Inner exception for the stack trace.</param>
         public TransportException(string message, System.Exception inner)
             : base(message, inner)
         {
