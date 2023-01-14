6a85263f1bcd44f6ae146c0bddfaf91b08cf9a5e
i80287 <92759021+i80287@users.noreply.github.com>
2023-01-14 22:28:01 +0300

    add fourth TransportException ctor

--- EKRLib/TransportException.cs
+++ EKRLib/TransportException.cs
@@ -1,14 +1,18 @@
+using System;
+using System.Runtime.Serialization;
+
 namespace EKRLib
 {
     /// <summary>
     /// Represents a class for the <see cref="EKRLib"/> library special exceptions.
     /// </summary>
-    public sealed class TransportException : System.Exception
+    public class TransportException : Exception
     {
         /// <summary>
         /// Initializes a new <see cref="TransportException"/> with empty message.
         /// </summary>
         public TransportException()
+            : base()
         {
         }
 
@@ -27,8 +31,30 @@ namespace EKRLib
         /// </summary>
         /// <param name="message">Exception message.</param>
         /// <param name="inner">Inner exception for the stack trace.</param>
-        public TransportException(string message, System.Exception inner)
+        public TransportException(string message, Exception inner)
             : base(message, inner)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TransportException"/> class with serialized data.
+        /// </summary>
+        /// <param name="info">
+        /// The System.Runtime.Serialization.SerializationInfo that holds
+        /// the serialized object data about the exception being thrown.
+        /// </param>
+        /// <param name="context">
+        /// The System.Runtime.Serialization.StreamingContext that contains
+        /// contextual information about the source or destination.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if info is null.
+        /// </exception>
+        /// <exception cref="SerializationException">
+        /// Thrown if the class name is null or <see cref="Exception.HResult"/> is zero (0).
+        /// </exception>
+        protected TransportException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
         {
         }
     }
