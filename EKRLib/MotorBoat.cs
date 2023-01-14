e18b33390b8b9acf92ad9bc4f1b7c7f953e73975
i80287 <92759021+i80287@users.noreply.github.com>
2023-01-14 22:28:38 +0300

    update MotorBoat

---
+++ EKRLib/MotorBoat.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace EKRLib
+{
+    /// <summary>
+    /// Represents a class <see cref="MotorBoat"/> inherited from
+    /// the abstract transport class <see cref="Transport"/>.
+    /// </summary>
+    public sealed class MotorBoat : Transport
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MotorBoat"/> class with provided <paramref name="model"/> and <paramref name="power"/>.
+        /// </summary>
+        /// <param name="model"><see cref="MotorBoat"/> model name.</param>
+        /// <param name="power"><see cref="MotorBoat"/> engine power.</param>
+        /// <exception cref="TransportException">
+        /// Thrown if model or power are in the incorrect format.
+        /// </exception>
+        public MotorBoat([DisallowNull] string model, uint power)
+            : base(model, power)
+        {
+        }
+
+        /// <summary>
+        /// Starts <see cref="MotorBoat"/> engine.
+        /// </summary>
+        /// <returns>Engine's sound.</returns>
+        [return: NotNull]
+        public override string StartEngine()
+            => $"{Model}: Brrrbr";
+
+        public override string ToString()
+            => "MotorBoat. " + base.ToString();
+    }
+}
